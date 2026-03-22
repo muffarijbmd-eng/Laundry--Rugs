f4aed1b8a6f4364f4536eb803814d1735892ba5e
muffarijbmd-eng <muffarij.bmd@gmail.com>
2026-03-22 15:05:13 +0200

    Create app/layout.tsx with content from the root level layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// app/layout.tsx
+// Content migrated from layout.tsx
+
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default Layout;
